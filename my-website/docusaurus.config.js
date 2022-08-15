964c1c8a53c3e4e2c3492a235e6cc6b023b45691
zacharybk <zacharybk@gmail.com>
2022-08-15 10:40:58 -0400

    removed bad puncutation

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -124,4 +124,4 @@ module.exports = {
   url: 'https://shiny-begonia-2c0a3a.netlify.app', // Url to your site with no trailing slash
   baseUrl: '/', // Base directory of your site relative to your repo
   // ...
-};;
+};
